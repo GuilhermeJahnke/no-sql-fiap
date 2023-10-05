708f331cc3ed1c1868753bd190ade891cf1f3563
Gustavo Guilherme <88010932+gustavoGui17@users.noreply.github.com>
2023-10-05 20:15:49 -0300

    CRUD produtcs
    Criando CRUD de produtos

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 import UserRepository from "./user.js";
-import ProductRepository from "./products.js";
+import productRepositry from "./products.js";
 
 const uri =
   "mongodb+srv://fiap:fiap123@cluster0.umje6.mongodb.net/?retryWrites=true&w=majority";
@@ -54,7 +54,6 @@ let client = new MongoClient(uri, {
 
   await userCRUD(db);
 
- 
   // Product
 
   // Orders
@@ -68,15 +67,14 @@ let client = new MongoClient(uri, {
 
 // run().catch(console.dir);
 
-
-async function userCRUD(db){
+async function userCRUD(db) {
   // ConnectDB
   const userRepositry = new UserRepository({ client: db });
 
-  console.log("\nConectado a tabela usuario com sucesso\n")
+  console.log("\nConectado a tabela usuario com sucesso\n");
 
   // CREATE
-  console.log("\nIniciando a criacao do Usuario com o metodo createUser\n")
+  console.log("\nIniciando a criacao do Usuario com o metodo createUser\n");
   await userRepositry.createUser({
     fullName: "Adalberto Alves",
     email: "adalbertoAlves@teste.com",
@@ -85,12 +83,12 @@ async function userCRUD(db){
   });
 
   // GETBYID
-  console.log("\nIniciando a busca do usuario com o metodo getByCPF\n")
+  console.log("\nIniciando a busca do usuario com o metodo getByCPF\n");
   let user = await userRepositry.getByCpf("12345678909");
-  console.log(user)
+  console.log(user);
 
   // EDIT
-  console.log("\nIniciando a edicao do usuario com o metodo editUser\n")
+  console.log("\nIniciando a edicao do usuario com o metodo editUser\n");
   await userRepositry.editUser({
     id: user._id,
     fullName: "FIAP teste ",
@@ -98,34 +96,68 @@ async function userCRUD(db){
     password: "123456",
     cpf: "12345678909",
   });
-  
+
   // GETBYID
-  console.log("\nIniciando a busca do usuario atualizado com o metodo getByCPF\n")
+  console.log(
+    "\nIniciando a busca do usuario atualizado com o metodo getByCPF\n"
+  );
   let updatedUser = await userRepositry.getByCpf("12345678909");
 
-  console.log(updatedUser)
+  console.log(updatedUser);
 
   // GETALL
-  console.log("\nIniciando a busca de todos os usuarios com o metodo getAll\n")
+  console.log("\nIniciando a busca de todos os usuarios com o metodo getAll\n");
   let users = await userRepositry.getAll();
-  await users.forEach(user => {
-    console.log(user)
+  await users.forEach((user) => {
+    console.log(user);
   });
 
   // DELETE
-  console.log("\nIniciando a delecao do usuario com o metodo deleteUser\n")
-  
+  console.log("\nIniciando a delecao do usuario com o metodo deleteUser\n");
+
   await userRepositry.deleteUser(updatedUser._id);
 
   // GETALL
-  console.log("\nIniciando a busca dos usuarios com o metodo getAll\n")
+  console.log("\nIniciando a busca dos usuarios com o metodo getAll\n");
 
-  let finalUsers =  await userRepositry.getAll();
+  let finalUsers = await userRepositry.getAll();
 
-  console.log("\nUsuarios encontrados\n")
+  console.log("\nUsuarios encontrados\n");
 
-  await finalUsers.forEach(user => {
-    console.log(user)
+  await finalUsers.forEach((user) => {
+    console.log(user);
   });
 
+  async function productsCRUD(db) {
+    // ConnectDB
+    const productRepositry = new productRepositry({ client: db });
+
+    console.log("\nConectado a tabela de produtos com sucesso\n");
+  }
+
+  // CREATE
+  console.log("\nIniciando a criacao de produtos com o metodo createProduct\n");
+  await productRepositry.createProduct({
+    nameOfProduct: "Batata",
+    price: "20.00",
+  });
+
+  // GETALL
+  console.log("\nIniciando a busca de todos os produtos com o metodo getAll\n");
+  let products = await productRepositry.getAll();
+  await products.forEach((product) => {
+    console.log(product);
+  });
+
+  // EDIT
+  console.log("\nIniciando a edicao do produto com o metodo editProduct\n");
+  await productRepositry.editProduct({
+    nameOfProduct: "Arroz",
+    price: "30.00",
+  });
+
+  // DELETE
+  console.log("\nIniciando a delecao de produtos com o metodo deleteUser\n");
+
+  await productRepositry.deleteProduct(nameOfProduct);
 }
