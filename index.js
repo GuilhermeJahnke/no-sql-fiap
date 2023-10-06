638ca7775c708829c1010bc11166b25a3cfe0256
Guilherme Jahnke Vieira <85601132+GuilhermeJahnke@users.noreply.github.com>
2023-10-05 21:16:26 -0300

    Orders (#2)
    feat(no-issue):( finished order crud )

--- index.js
+++ index.js
@@ -1,131 +1,38 @@
-// const MongoClient = require("mongodb").MongoClient;
-
-// Configurações do servidor MongoDB
-// const url = "mongodb://localhost:27017"; // URL do servidor MongoDB
-// const dbName = "mydatabase"; // Nome do banco de dados
-
-// const client = new MongoClient(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// // });
-
-// const { performCRUDOperations } = require("./orders");
-
 import { MongoClient, ServerApiVersion } from "mongodb";
-
+import OrderRepository from "./orders.js";
 import UserRepository from "./user.js";
-import ProductRepository from "./products.js";
-
-const uri =
-  "mongodb+srv://fiap:fiap123@cluster0.umje6.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-let client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-// const Product = new ProductRepository({ client });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     client = await client.connect();
-//     // Send a ping to confirm a successful connection
-//     // let mongoClient = await
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//     console.log(client);
-//     await performCRUDOperations(client.db("fiap"));
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
 
 (async () => {
-  await client.connect();
-  const db = client.db("fiap");
-  console.log("Connected correctly to server");
+  const uri =
+    "mongodb+srv://fiap:fiap123@cluster0.umje6.mongodb.net/?retryWrites=true&w=majority";
 
-  await userCRUD(db);
+  let client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  client = await client.connect();
 
- 
-  // Product
+  try {
+    const db = client.db("fiap");
+    console.log("Connected correctly to server");
+    
+    // Order
+    const orderRepositry = new OrderRepository({ client: db });
+    await orderRepositry.execute();
 
-  // Orders
+    // User
+    const userRepositry = new UserRepository({ client: db });
+    await userRepositry.execute();
 
-  // Finalizando conexão
-  setTimeout(async () => {
+    // Products
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("Disconnecting");
     await client.close();
-  }, 1500);
-  console.log("Disconnected correctly to server");
+    process.exit(0);
+  }
 })();
-
-// run().catch(console.dir);
-
-
-async function userCRUD(db){
-  // ConnectDB
-  const userRepositry = new UserRepository({ client: db });
-
-  console.log("\nConectado a tabela usuario com sucesso\n")
-
-  // CREATE
-  console.log("\nIniciando a criacao do Usuario com o metodo createUser\n")
-  await userRepositry.createUser({
-    fullName: "Adalberto Alves",
-    email: "adalbertoAlves@teste.com",
-    password: "123456",
-    cpf: "12345678909",
-  });
-
-  // GETBYID
-  console.log("\nIniciando a busca do usuario com o metodo getByCPF\n")
-  let user = await userRepositry.getByCpf("12345678909");
-  console.log(user)
-
-  // EDIT
-  console.log("\nIniciando a edicao do usuario com o metodo editUser\n")
-  await userRepositry.editUser({
-    id: user._id,
-    fullName: "FIAP teste ",
-    email: "fiapteste@teste.com",
-    password: "123456",
-    cpf: "12345678909",
-  });
-  
-  // GETBYID
-  console.log("\nIniciando a busca do usuario atualizado com o metodo getByCPF\n")
-  let updatedUser = await userRepositry.getByCpf("12345678909");
-
-  console.log(updatedUser)
-
-  // GETALL
-  console.log("\nIniciando a busca de todos os usuarios com o metodo getAll\n")
-  let users = await userRepositry.getAll();
-  await users.forEach(user => {
-    console.log(user)
-  });
-
-  // DELETE
-  console.log("\nIniciando a delecao do usuario com o metodo deleteUser\n")
-  
-  await userRepositry.deleteUser(updatedUser._id);
-
-  // GETALL
-  console.log("\nIniciando a busca dos usuarios com o metodo getAll\n")
-
-  let finalUsers =  await userRepositry.getAll();
-
-  console.log("\nUsuarios encontrados\n")
-
-  await finalUsers.forEach(user => {
-    console.log(user)
-  });
-
-}
--- user.js
+++ user.js
@@ -1,11 +1,85 @@
 class UserRepository {
   constructor({ client }) {
-    this.client = client;
+    this.client = client.collection("user");
+  }
+
+  async execute() {
+    // ConnectDB
+    console.log("\nConectado a tabela usuario com sucesso");
+
+    // CREATE
+    console.log("\nIniciando a criacao de 3 Usuarios com o metodo createUser\n");
+    await this.createUser({
+      fullName: "Adalberto Alves",
+      email: "adalbertoAlves@teste.com",
+      password: "123456",
+      cpf: "12345678909",
+    });
+    await this.createUser({
+      fullName: "Fulano de Tal",
+      email: "fulanodetal@teste.com",
+      password: "123456",
+      cpf: "43542227013",
+    });
+    await this.createUser({
+      fullName: "Ciclano de Tal",
+      email: "ciclanodetal@teste.com",
+      password: "123456",
+      cpf: "73456377002",
+    });
+
+    console.log(
+      "\nIniciando a busca de todos os usuarios com o metodo getAll\n"
+    );
+
+    let firstSearch = await this.getAll();
+
+    console.log(await firstSearch.toArray());
+
+    // EDIT
+    console.log("\nIniciando a edicao do usuario com o metodo editUser\n");
+    await this.editUser({
+      fullName: "FIAP teste ",
+      email: "fiapteste@teste.com",
+      password: "123456",
+      cpf: "12345678909",
+    });
+    await this.editUser({
+      fullName: "FIAP teste 2",
+      email: "fiapteste2@teste.com",
+      password: "123456",
+      cpf: "43542227013",
+    });
+
+    // GETALL
+    console.log(
+      "Iniciando a busca de todos os usuarios com o metodo getAll\n"
+    );
+    let users = await this.getAll();
+
+    console.log(await users.toArray());
+
+
+    // DELETE
+    console.log("\nDeletando todos os usuarios criado\n");
+    
+    await this.deleteUser("12345678909");
+    await this.deleteUser("43542227013");
+    await this.deleteUser("73456377002");
+
+    console.log("\nIniciando a busca dos usuarios com o metodo getAll\n");
+    
+    let finalUsers = await this.getAll();
+
+    console.log("Usuarios encontrados\n");
+
+    console.log(await finalUsers.toArray())
+
+    console.log("\nFinalizando a execucao do UserRepository\n");
   }
 
   async createUser({ cpf, email, password, fullName }) {
     try {
-      const userCollections = this.client.collection("user");
 
       const newUser = {
         cpf,
@@ -14,7 +88,7 @@ class UserRepository {
         fullName,
       };
 
-      await userCollections.insertOne(newUser);
+      await this.client.insertOne(newUser);
 
       return console.log("Usuario criado com sucesso!");
     } catch (error) {
@@ -24,9 +98,8 @@ class UserRepository {
 
   async getAll() {
     try {
-      const userCollections = this.client.collection("user");
 
-      const users = await userCollections.find({});
+      const users = await this.client.find({});
 
       return users;
     } catch (error) {
@@ -34,47 +107,41 @@ class UserRepository {
     }
   }
 
-  async getByCpf(cpf) {
+  async editUser({ cpf, email, password, fullName }) {
     try {
-      const userCollections = this.client.collection("user");
+      let user = {
+        cpf,
+        email,
+        password,
+        fullName
+      }
 
-      const user = await userCollections.findOne({ cpf: cpf });
+      console.log("Atualizando o usuario com estes dados:", JSON.stringify(user, null, 2));
 
-      return user;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
-
-  async editUser({ id, cpf, email, password, fullName }) {
-    try {
-      const userCollections = this.client.collection("user");
-
-      await userCollections.findOneAndUpdate(
+      await this.client.findOneAndUpdate(
         {
-          "_id": id,
+          cpf: cpf,
         },
         {
           $set: {
-            "cpf": cpf,
-            "email": email,
-            "password": password,
-            "fullName": fullName,
+            cpf: cpf,
+            email: email,
+            password: password,
+            fullName: fullName,
           },
         }
       );
 
-      return console.log("Usuario atualizado com sucesso!");
+      return console.log("\nUsuario atualizado com sucesso!\n");
     } catch (error) {
       return console.log(error);
     }
   }
 
-  async deleteUser(id) {
+  async deleteUser(cpf) {
     try {
-      const userCollections = this.client.collection("user");
-     
-      await userCollections.findOneAndDelete({ "_id" : id });
+
+      await this.client.findOneAndDelete({ cpf: cpf });
 
       return console.log("Usuario deletado com sucesso!");
     } catch (error) {
